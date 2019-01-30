f3e94524307b92b82149aa178941188580becbfa
某M <mmnstar@hotmail.com>
2019-01-30 16:47:31 +0800

    20190130

---
+++ mVote/mVote/Default.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Templates/Site1.Master" CodeBehind="Default.aspx.vb" Inherits="mVote._Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ mVote/mVote/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 有关程序集的常规信息通过下列特性集
+' 控制。更改这些特性值可修改
+' 与程序集关联的信息。
+
+' 检查程序集特性的值
+<Assembly: AssemblyTitle("mVote")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("mVote")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
+<Assembly: Guid("78b9f578-cb68-4bc1-aa3f-e40d402397d7")>
+
+' 程序集的版本信息由下列四个值组成:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' 可以指定所有值，也可以使用“内部版本号”和“修订号”的默认值，
+' 方法是按如下所示使用 "*":
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
